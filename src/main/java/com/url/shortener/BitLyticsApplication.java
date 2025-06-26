18d0e57b5509b9ce82b6bef919bd1ae1aa339c75
AlinRenit <alinrenit2006@gmail.com>
2025-06-26 12:32:07 +0400

    Early Prototype of BitLytics

---
+++ src/main/java/com/url/shortener/BitLyticsApplication.java
@@ -0,0 +1,13 @@
+package com.url.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitLyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitLyticsApplication.class, args);
+    }
+
+}
